5203d09500939b2918c682240b8e922f7aacc3c9
Cselfy <cselfymain@gmail.com>
2024-11-03 19:24:07 +0200

    A more pleasant look ( functions )

--- hw/main.js
+++ hw/main.js
@@ -1,38 +1,47 @@
-// 1 
-// let name = prompt("Enter your name: ");
-// alert("Hi! " + name + "!");
 
-// 2    
-// let userBirthYear = +prompt("Enter your birth year: ");
-// const currentYear = 2024;
-// let userAge = currentYear - userBirthYear;
-// alert("You are " + userAge + " years old.");
+function Task1() {
+    let name = prompt("Enter your name: ");
+    alert("Hi! " + name + "!");
+}
 
-// 3
-// let squareSide = +prompt("Enter the side of the square: ");
-// let squarePerimeter = 4 * squareSide;
-// alert("The perimeter of square is: " + squarePerimeter + " cm.");
+function Task2() {
+    let userBirthYear = +prompt("Enter your birth year: ");
+    const currentYear = 2024;
+    let userAge = currentYear - userBirthYear;
+    alert("You are " + userAge + " years old.");
+}
 
-// 4
-// let radius = +prompt("Enter the radius of the circle: ");
-// let circleArea = 3.14 * radius * radius;
-// alert("Area of the circle is: " + circleArea + " cm^2.");
+function Task3() {
+    let squareSide = +prompt("Enter the side of the square: ");
+    let squarePerimeter = 4 * squareSide;
+    alert("The perimeter of square is: " + squarePerimeter + " cm.");
+}
 
-// 5
-// let distance = +prompt("Enter the distance between two cities (in km): ");
-// let time = +prompt("Enter your desired time to reach your destination (in hours): ");
-// let speed = distance / time; 
-// alert("You should travel at " + Math.round(speed) + " km/h to reach your destination on time."); // Math.round() 56.5555555....
+function Task4() {
+    let radius = +prompt("Enter the radius of the circle: ");
+    let circleArea = 3.14 * radius * radius;
+    alert("Area of the circle is: " + circleArea + " cm^2.");
+}
 
-// 6
-// const exchangeRate = 0.92;
-// let dollars = +prompt("Enter the amount of $: ");
-// let result = dollars * exchangeRate; 
-// alert(dollars + " $" + " = " + result.toFixed(2) + " €"); // 115.25...
+function Task5() {
+    let distance = +prompt("Enter the distance between two cities (in km): ");
+    let time = +prompt("Enter your desired time to reach your destination (in hours): ");
+    let speed = distance / time; 
+    alert("You should travel at " + Math.round(speed) + " km/h to reach your destination on time."); // 55.555555...
+}
 
-// 7 
-const fSize = 820;
-let usb = +prompt("Enter your USB flash drive space: ");
-let usbMB = usb * 1024; 
-let result = usbMB / fSize; 
-alert("You will be able to fit: " + Math.floor(result) + " files on your usb flash drive");
+function Task6() {
+    const exchangeRate = 0.92;
+    let dollars = +prompt("Enter the amount of $: ");
+    let result = dollars * exchangeRate; 
+    alert(dollars + " $" + " = " + result.toFixed(2) + " €"); //92.01
+}
+
+function Task7() {
+    const fSize = 820; 
+    let usb = +prompt("Enter your USB flash drive space in GB: ");
+    let usbMB = usb * 1024; 
+    let result = usbMB / fSize; 
+    alert("You will be able to store: " + Math.floor(result) + " files on your USB flash drive");
+}
+
