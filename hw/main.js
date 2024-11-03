a9932ed778cf7835e2a9b79ba311c06ccc098ba3
Cselfy <cselfymain@gmail.com>
2024-11-03 20:06:51 +0200

    Added 8, 9, 10 tasks

--- hw/main.js
+++ hw/main.js
@@ -1,4 +1,3 @@
-
 function Task1() {
     let name = prompt("Enter your name: ");
     alert("Hi! " + name + "!");
@@ -45,3 +44,38 @@ function Task7() {
     alert("You will be able to store: " + Math.floor(result) + " files on your USB flash drive");
 }
 
+function Task8()
+{
+    let wallet = +prompt("Enter amount of money you have in your wallet (UAH): ");
+    let chocoPrice = +prompt("Enter chocolate price (UAH): ");
+    let chocoAmount = Math.floor(wallet / chocoPrice); 
+    let change = wallet - (chocoAmount * chocoPrice);
+    alert("With " + wallet + " UAH, you can buy " + chocoAmount + " chocolates, and your change is: " + change + " UAH");
+}
+
+function Task10()
+{
+    let int = prompt("Enter an integer: "); 
+    let result = (int % 2 == 0) ? "even" : "odd";
+    alert("The number is: " + result);
+}
+
+function Task9() { // horrific task !
+    let num = +prompt("Enter a three-digit number: ");
+    let hundreds = Math.floor(num / 100);
+    let tens = Math.floor((num % 100) / 10); 
+    let units = num % 10; 
+    let reversedNum = (units * 100) + (tens * 10) + hundreds;
+
+    alert("The palindrome of " + num + " is: " + reversedNum);
+}
+// Task1()
+// Task2()
+// Task3()
+// Task4()
+// Task5()
+// Task6()
+// Task7()
+// Task8()
+// Task10()
+Task9()
